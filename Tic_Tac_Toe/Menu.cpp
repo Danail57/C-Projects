83bec6fb080bca4bec29a0ea89c36c68a73e7201
Danail Donkov <danail.donkov5@gmail.com>
2025-12-14 22:21:39 +0200

    Update and rename file.cpp to Menu.cpp

---
+++ Tic_Tac_Toe/Menu.cpp
@@ -0,0 +1,11 @@
+int menu()
+{
+	int choice;
+	cout << "==== TIC TAC TOE ====\n";
+	cout << "1. Player vs Player\n";
+	cout << "2. Player vs Computer\n";
+	cout << "0. Exit\n";
+	cout << "Choose option: ";
+	cin >> choice;
+	return choice;
+}
