85968414259e2615ab2f29608b4d040f718fd535
Danail Donkov <danail.donkov5@gmail.com>
2026-01-12 21:16:14 +0200

    Create keyboard_input.cpp

---
+++ Snake_Game/keyboard_input.cpp
@@ -0,0 +1,21 @@
+void input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			if (dir != RIGHT) dir = LEFT;
+			break;
+		case 'd':
+			if (dir != LEFT) dir = RIGHT;
+			break;
+		case 'w':
+			if (dir != DOWN) dir = UP;
+			break;
+		case 's':
+			if (dir != UP) dir = DOWN;
+			break;
+		}
+	}
+}
